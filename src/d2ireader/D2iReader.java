5c5f8b1258a50ea13c5fc3a5724da3724ec7eaaf
Jose M. Soto Acosta <jomisoac@gmail.com>
2017-04-25 02:02:15 -0500

    first commit

---
+++ src/d2ireader/D2iReader.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package d2ireader;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class D2iReader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Reader a = new Reader("E:\\Escritorio\\editar\\datos.d2i") {};
+//        a._aLang.entrySet().stream().forEach((entry) -> {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        });
+
+        System.out.println("*****************Langs Party*************************\n");
+
+        for (int i = 1; i <= 1000445; i++) {
+            System.out.println(i+" "+a.getText(i));
+        }
+        
+//        int i = 1;
+//        while (a.getText(i) != null) {
+//            System.out.println(i+" "+a.getText(i));
+//            i++;
+//        }
+    }
+    
+}
